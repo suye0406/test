2e61b80bfc793a4f1d9ae2c910c9c42595172709
suyuchen <suyuchencom@163.com>
2019-06-16 16:55:38 +0800

    好的
    Signed-off-by: suyuchen <suyuchencom@163.com>

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    baseUrl: './',
+    lintOnSave: false,
+    devServer: {
+            port: 8080,     // 端口
+    },
+        // devServer: {
+        //     port: 1314,     // 端口
+        // },
+        // lintOnSave: false   // 取消 eslint 验证
+};
